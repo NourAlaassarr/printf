e5e7acb719db88787e1187164d6b75d73a900669
itsalaassar <itsalaassar@gmail.com>
2023-04-25 23:04:42 +0200

    updated

---
+++ write.c
@@ -0,0 +1,42 @@
+#include "main.h"
+#include <stdio.h>
+
+/**
+ *_putchar - function to print char
+ **ch: character to be printed
+ *
+ * Return: int
+ */
+
+int _putchar(int ch )
+{
+ static int i;
+ static char buffer[BUF_SIZE];
+ if (ch == BUF_FLUSH || i >= BUF_SIZE)
+ {
+     write(1, buffer, i);
+     i = 0;
+ }
+ if (ch != BUF_FLUSH)
+ {
+     buffer[i++] = ch;
+
+ }
+ return(1);
+}
+
+/**
+ * _puts - print
+ * @s: char
+ *
+ * Return: printed int
+ */
+
+int _puts(char*s)
+{
+    char *c = s;
+    while(*s)
+      _putchar(*s++);
+    return (s - c);
+
+}
