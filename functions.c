08a91a4573be981428748599f10d67509b06dd93
NashwaKandil <nashwakandel@gmail.com>
2023-04-26 18:54:30 -0700

    fixes

--- functions.c
+++ functions.c
@@ -1,82 +1,136 @@
 #include "main.h"
 
 /**
- * converting - function to convert integers to strings
- * @number: number entered
- * @base: base of number
- * @flags: flags entered
- * @p: parameter struct
- * Return: string
+ * print_hexanum - Prints a hexadecimal number in lower or upper
+ * @types: arguments list
+ * @map_to: Array of values to map the number to
+ * @buff: printing array
+ * @flags: active flags
+ * @flag_ch: Calculates active flags
+ * @width: width calculated
+ * @precision: precision calculated
+ * @size: size calculated
+ * Return: printed nummbers
  */
 
-char *converting(long int number, int base, int flags, params_ *p)
+int print_hexanum(va_list types, char map_to[],
+char buff[], int flags, char flag_ch, int width, int precision, int size);
 {
-	static char *arr;
-	static char buff[50];
-	char sign = 0;
-	char *ptr;
-	unsigned long n = number;
-	(void)p;
+	int j = BUFF_SIZE - 2;
+	unsigned long int num = va_arg(types, unsigned long int);
+	unsigned long int init_num = num;
 
-	if (!(flags & CONVERT_UNSIGNED) && number < 0)
+	UNUSED(width);
+
+	num = converting_unsign(num, size);
+
+	if (num == 0)
+		buff[j--] = '0';
+
+	buff[BUFF_SIZE - 1] = '\0';
+
+	while (num > 0)
 	{
-		n = -number;
-		sign = '-';
+		buff[j--] = map_to[num % 16];
+		num /= 16;
 	}
-	arr = flags & CONVERT_LOWERCASE ? "0123456789abcdef" : "0123456789ABCDEF";
-	ptr = &buff[49];
-	*ptr = '\0';
 
-	do  {
-		*--ptr = arr[n % base];
-		n /= base;
-	} while (n != 0);
+	if (flags & F_HASH && init_num != 0)
+	{
+		buff[j--] = flag_ch;
+		buff[j--] = '0';
+	}
 
-	if (sign)
-		*--ptr = sign;
-	return (ptr);
+	j++;
+
+	return (write_unisgned(0, j, buff, flags, width, precision, size));
 }
 
 
 /**
  * print_unsigned - prints unsigned integers
- * @ap: pointer to argument
- * @p: parameters structure
- * Return: printed integers
+ * @types: arguments list
+ * @buff: printing array
+ * @flags: active flags
+ * @width: width calculated
+ * @precision: precision calculated
+ * @size: size calculated
+ * Return: printed nummbers
  */
 
-int print_unsigned(va_list ap, params_ *p)
+int print_unsigned(va_list types, char buff[],
+	int flags, int width, int precision, int size);
 {
-	unsigned long lo;
+	int j = BUFF_SIZE - 2;
+	unsigned long int num = va_arg(types, unsigned long int);
 
-	if (p->lmodifier)
-		lo = (unsigned long)va_arg(ap, unsigned long);
-	else if (p->hmodifier)
-		lo = (unsigned short int)va_arg(ap, unsigned int);
-	else
-		lo = (unsigned int)va_arg(ap, unsigned int);
-	p->unsign = 1;
-	return (printnumbers(converting(lo, 10, CONVERT_UNSIGNED, p), p));
+	num = converting_unsign(num, size);
+
+	if (num == 0)
+		buff[j--] = '0';
+
+	buff[BUFF_SIZE - 1] = '\0';
+
+	while (num > 0)
+	{
+		buff[j--] = (num % 10) + '0';
+		num /= 10;
+	}
+
+	j++;
+
+	return (write_unsigned(0, j, buff, flags, width, precision, size));
 }
 
 
 /**
  * print_address - prints an address
- * @ap: pointer to argument
- * @p: parameters structure
- * Return: printed address
+ * @types: arguments list
+ * @buff: printing array
+ * @flags: active flags
+ * @width: width calculated
+ * @precision: precision calculated
+ * @size: size calculated
+ * Return: printed nummbers
  */
 
-int print_address(va_list ap, params_ *p)
+int print_address(va_list types, char buff[],
+	int flags, int width, int precision, int size);
 {
-	unsigned long int m = va_arg(ap, unsigned long int);
-	char *s;
+	char extra_c = 0, padd = ' ';
+	int ind = BUFF_SIZE - 2, len = 2, padd_start = 1; /* length=2, for '0x' */
+	unsigned long num_addrs;
+	char map_to[] = "0123456789abcdef";
+	void *addrs = va_arg(types, void *);
 
-	if (!m)
-		return (_puts("(nil)"));
+	UNUSED(width);
+	UNUSED(size);
 
-	s = converting(m, 16, CONVERT_UNSIGNED | CONVERT_LOWERCASE, p);
-	*--s = 'x';
-	*--s = '0';
-	return (printnumbers(s, p));
+	if (addrs == NULL)
+		return (write(1, "(nil)", 5));
+
+	buff[BUFF_SIZE - 1] = '\0';
+	UNUSED(precision);
+
+	num_addrs = (unsigned long)addrs;
+
+	while (num_addrs > 0)
+	{
+		buff[ind--] = map_to[num_addrs % 16];
+		num_addrs /= 16;
+		len++;
+	}
+
+	if ((flags & F_ZERO) && !(flags & F_MINUS))
+		padd = '0';
+	if (flags & F_PLUS)
+		extra_c = '+', len++;
+	else if (flags & F_SPACE)
+		extra_c = ' ', len++;
+
+	ind++;
+
+	/*return (write(1, &buffer[i], BUFF_SIZE - i - 1));*/
+	return (pointer_writing(buff, ind, len,
+		width, flags, padd, extra_c, padd_start));
 }
